1b7c94d2800ad3067e111f6b3b260f886dc3dcfd
AlokSachan <alok.sachan.83@gmail.com>
2022-11-10 16:45:10 +0530

    detect updated property value using refeshScope

--- src/main/java/com/alok/accounts/AccountsServiceApplication.java
+++ src/main/java/com/alok/accounts/AccountsServiceApplication.java
@@ -3,12 +3,13 @@ package com.alok.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.alok.accounts.controller"),
  @ComponentScan("com.alok.accounts.adaptor")})
 @EntityScan("com.alok.accounts.entity")
